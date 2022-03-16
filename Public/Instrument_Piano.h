3d9ce68aa61d7b6f70587f971d0b336a3dfdd8f7
Gwanghyun Choi <66342017+helirang@users.noreply.github.com>
2022-03-16 22:56:33 +0900

    Update Instrument_Piano.h

--- Public/Instrument_Piano.h
+++ Public/Instrument_Piano.h
@@ -15,10 +15,14 @@ class MINIPROJECT_PIANO_API AInstrument_Piano : public AInstrument
 {
 	GENERATED_BODY()
 private:
+	//건반의 메테리얼을 변경하는 함수
 	void ChangeKeyboardMetarial(int soundNum);
 public:
+	//Instrument의 연주 기능을 오버라이딩하는 함수
 	virtual void PlayInstrument(int soundNum) override;
+	//BeginPlay에서 world에 생성되어있는 piano 건반들을 keyboardArr에 셋팅한다. @todo Instrument에서 건반을 생성하게 변경
 	virtual void BeginPlay() override;
 private:
+	//메테리얼을 변경할 Piano 액터들의 배열
 	TArray<APiano*> keyboardArr;
 };
